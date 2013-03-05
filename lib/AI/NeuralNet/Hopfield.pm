fcc60a464f6bcfd43954b18935cebc28f602ca2f
Felipe da Veiga Leprevost <leprevost@cpan.org>
2013-03-05 04:17:41 +0000

    Import of LEPREVOST/AI-NeuralNet-Hopfield-0.09 from CPAN.
    gitpan-cpan-distribution: AI-NeuralNet-Hopfield
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         LEPREVOST/AI-NeuralNet-Hopfield-0.09.tar.gz
    gitpan-cpan-author:       LEPREVOST
    gitpan-cpan-maturity:     released

--- lib/AI/NeuralNet/Hopfield.pm
+++ lib/AI/NeuralNet/Hopfield.pm
@@ -13,11 +13,11 @@ AI::NeuralNet::Hopfield - A simple Hopfiled Network Implementation.
 
 =head1 VERSION
 
-Version 0.08
+Version 0.09
 
 =cut
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 has 'matrix' => ( is => 'rw', isa => 'Math::SparseMatrix');
 
